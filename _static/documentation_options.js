5223a1ddaa0964e40b6505b7273d3297487d3e49
jcrist <jcrist@users.noreply.github.com>
2021-06-28 20:50:14 +0000

    deploy: 64d7334c16eefc86b5d987081a4e689f8328fb2b

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0+untagged.1.g4cebab7',
+    VERSION: '0+untagged.1.g64d7334',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
