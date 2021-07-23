48b460c7b1bb4f4f1b2943bd7352c8abd7679bdb
Jim Crist-Harif <jcristharif@gmail.com>
2021-07-23 15:50:42 -0500

    Small opts

--- msgspec/core.c
+++ msgspec/core.c
@@ -4997,7 +4997,7 @@ json_encode_str(EncoderState *self, PyObject *obj) {
         /* Scan through until a character needs to be escaped */
         char c = buf[i];
         char escape = escape_table[(uint8_t)c];
-        if (escape == 0) continue;
+        if (MP_LIKELY(escape == 0)) continue;
 
         /* Write the fragment that doesn't need to be escaped */
         if (start < i) {
@@ -5005,14 +5005,16 @@ json_encode_str(EncoderState *self, PyObject *obj) {
         }
 
         /* Write the escaped character */
-        size_t size = escape == 'u' ? 6 : 2;
         char escaped[6] = {'\\', escape, '0', '0'};
-        if (escape == 'u') {
+        if (MP_UNLIKELY(escape == 'u')) {
             static const char* const hex = "0123456789abcdef";
             escaped[4] = hex[c >> 4];
             escaped[5] = hex[c & 0xF];
+            if (mp_write(self, escaped, 6) < 0) return -1;
         }
-        if (mp_write(self, escaped, size) < 0) return -1;
+        else {
+            if (mp_write(self, escaped, 2) < 0) return -1;
+        }
         start = i + 1;
     }
     /* Write the last unescaped fragment (if any) */
