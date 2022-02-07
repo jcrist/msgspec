3414a8b62d45a6edf425f7a6e19c179945ac5c1c
Jim Crist-Harif <jcristharif@gmail.com>
2022-02-07 00:14:56 -0600

    Update docs
    - Redesign docs using furo theme
    - Expand benchmark to include more libraries
    - Redo docs to better fit new multi-protocol (JSON & msgpack) support.
    - Add logo

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,39 +1,33 @@
 import msgspec
 
 project = "msgspec"
-copyright = "2021, Jim Crist-Harif"
+copyright = "2022, Jim Crist-Harif"
 author = "Jim Crist-Harif"
-release = version = msgspec.__version__
 
-html_theme = "alabaster"
+html_theme = "furo"
+html_title = ""
+templates_path = ["_templates"]
+html_static_path = ["_static"]
+html_theme_options = {
+    "light_logo": "msgspec-logo-light.svg",
+    "dark_logo": "msgspec-logo-dark.svg",
+    "sidebar_hide_name": True,
+}
+
 extensions = [
     "sphinx.ext.autodoc",
     "sphinx.ext.napoleon",
     "sphinx.ext.extlinks",
     "sphinx.ext.intersphinx",
+    "sphinx_copybutton",
 ]
 intersphinx_mapping = {"python": ("https://docs.python.org/3", None)}
 napoleon_numpy_docstring = True
 napoleon_google_docstring = False
 default_role = "obj"
-pygments_style = "sphinx"
-
-templates_path = ["_templates"]
-html_static_path = ["_static"]
-html_theme_options = {
-    "description": "A fast and friendly MessagePack library",
-    "github_button": True,
-    "github_count": False,
-    "github_user": "jcrist",
-    "github_repo": "msgspec",
-    "travis_button": False,
-    "show_powered_by": False,
-    "page_width": "960px",
-    "sidebar_width": "200px",
-    "code_font_size": "0.8em",
-}
-html_sidebars = {"**": ["about.html", "navigation.html", "help.html", "searchbox.html"]}
 extlinks = {
     "issue": ("https://github.com/jcrist/msgspec/issues/%s", "Issue #"),
     "pr": ("https://github.com/jcrist/msgspec/pull/%s", "PR #"),
 }
+copybutton_prompt_text = r">>> |\.\.\. |\$ "
+copybutton_prompt_is_regexp = True
