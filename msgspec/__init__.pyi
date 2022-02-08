40e5910d13d39436dbf9f5e5b30a6d57ad7937e3
Jim Crist-Harif <jcristharif@gmail.com>
2022-02-08 09:27:39 -0600

    Add pyright support
    Uses `__dataclass_transform__` for better pyright integration. Still
    need a mypy plugin to catch `__init__` errors in mypy.

--- msgspec/__init__.pyi
+++ msgspec/__init__.pyi
@@ -1,5 +1,28 @@
-class Struct:
-    def __init__(self, *args, **kwargs) -> None: ...
+from typing import Type
+from typing import Callable, Tuple, Any, Union, TypeVar
+
+# Use `__dataclass_transform__` to catch more errors under pyright. Since we don't expose
+# the underlying metaclass, hide it under an underscore name. See
+# https://github.com/microsoft/pyright/blob/main/specs/dataclass_transforms.md
+# for more information.
+
+_T = TypeVar("_T")
+
+def __dataclass_transform__(
+    *,
+    eq_default: bool = True,
+    order_default: bool = False,
+    kw_only_default: bool = False,
+    field_descriptors: Tuple[Union[type, Callable[..., Any]], ...] = (()),
+) -> Callable[[_T], _T]: ...
+@__dataclass_transform__()
+class __StructMeta(type):
+    def __new__(
+        cls: Type[type], name: str, bases: tuple, classdict: dict
+    ) -> "__StructMeta": ...
+
+class Struct(metaclass=__StructMeta):
+    def __init__(self, *args: Any, **kwargs: Any) -> None: ...
     def __init_subclass__(cls, asarray: bool = False, frozen: bool = False) -> None: ...
 
 class MsgspecError(Exception): ...
@@ -9,4 +32,4 @@ class EncodingError(MsgspecError): ...
 from . import msgpack
 from . import json
 
-__version__ : str
+__version__: str
