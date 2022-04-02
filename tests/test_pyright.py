f622dbcd126942d0a314ce82b307a3f18bbdebf4
Jim Crist-Harif <jcristharif@gmail.com>
2022-04-01 23:19:54 -0500

    Add pyright tests
    These tests only check for compatibility with the mypy checks, but
    pyright is also capable of catching errors mypy won't catch (without a
    mypy plugin).

---
+++ tests/test_pyright.py
@@ -0,0 +1,33 @@
+import re
+import os
+import subprocess
+
+import pytest
+
+pyright = pytest.importorskip("pyright")
+
+PATH = os.path.join(os.path.dirname(__file__), "basic_typing_examples.py")
+
+
+def test_pyright():
+    with open(PATH, "r") as fil:
+        ex_lines = fil.readlines()
+
+    result = pyright.run(PATH, stdout=subprocess.PIPE)
+    if result.returncode != 0:
+        assert False, f"Unexpected pyright error:\n{result.stdout}"
+    for line in result.stdout.decode().splitlines():
+        try:
+            _, lineno, _, msg = line.split(":", 3)
+        except ValueError:
+            continue
+        lineno = int(lineno)
+        pat = re.search("[\"'](.*)[\"']", msg)
+        typ = pat.groups()[0]
+        check = ex_lines[lineno - 1].split("#")[1].strip()
+        try:
+            exec(check, {"typ": typ})
+        except Exception:
+            assert (
+                False
+            ), f"Failed check at {PATH}:{lineno}: {check!r}, where 'typ' is {typ!r}"
