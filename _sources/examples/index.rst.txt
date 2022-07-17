54c0614f37c9fbaac8aa9a3eeed82da4e84837b2
jcrist <jcrist@users.noreply.github.com>
2022-07-17 18:07:59 +0000

    deploy: aaa22c84117d63e768e23fc1e8ca3506d278ad76

---
+++ _sources/examples/index.rst.txt
@@ -0,0 +1,8 @@
+Examples
+========
+
+Here we provide a few examples using ``msgspec`` to accomplish various tasks.
+
+.. toctree::
+
+    geojson.rst
--- _static/copybutton.js
+++ _static/copybutton.js
@@ -199,7 +199,7 @@ function formatCopyText(textContent, copybuttonPromptText, isRegexp = false, onl
 
 var copyTargetText = (trigger) => {
   var target = document.querySelector(trigger.attributes['data-clipboard-target'].value);
-  return formatCopyText(target.innerText, '>>> |\\.\\.\\. |\\$ ', true, true, true, true, '', '')
+  return formatCopyText(target.innerText, '>>> |\\.\\.\\. |\\$ |In \\[\\d*\\]: | {2,5}\\.\\.\\.: ', true, true, true, true, '', '')
 }
 
   // Initialize with a callback so we can modify the text before copy
