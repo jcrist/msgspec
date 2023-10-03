f9a76f041b2ec95c862ab97071f05c85907677cf
Jim Crist-Harif <jcristharif@gmail.com>
2023-10-03 01:09:16 -0500

    Enable CPython 3.12 release builds

--- setup.py
+++ setup.py
@@ -67,6 +67,7 @@ setup(
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
     ],
     extras_require=extras_require,
     license="BSD",
