79367262b104121ef932bd5cb05b4dceedc299a1
Ofek Lev <ofekmeister@gmail.com>
2025-10-26 18:51:23 -0400

    Remove setuptools hack

--- setup.py
+++ setup.py
@@ -24,18 +24,28 @@ if sys.platform == "win32" and sys.maxsize == (2**31 - 1):
     exit(1)
 
 
-SANITIZE = os.environ.get("MSGSPEC_SANITIZE", False)
-COVERAGE = os.environ.get("MSGSPEC_COVERAGE", False)
-DEBUG = os.environ.get("MSGSPEC_DEBUG", SANITIZE or COVERAGE)
+def is_enabled(name):
+    value = os.environ.get(name)
+    if value is None:
+        return False
+
+    return value.lower() in {"1", "true"}
+
+
+SANITIZE = is_enabled("MSGSPEC_SANITIZE")
+COVERAGE = is_enabled("MSGSPEC_COVERAGE")
+DEBUG = is_enabled("MSGSPEC_DEBUG") or SANITIZE or COVERAGE
 
 extra_compile_args = []
 extra_link_args = []
 if SANITIZE:
-    extra_compile_args.extend(["-fsanitize=address", "-fsanitize=undefined"])
-    extra_link_args.extend(["-lasan", "-lubsan"])
+    san = ("-fsanitize=address", "-fsanitize=undefined")
+    extra_compile_args.extend(san)
+    extra_link_args.extend(san)
 if COVERAGE:
-    extra_compile_args.append("--coverage")
-    extra_link_args.append("-lgcov")
+    cov = ("--coverage",)
+    extra_compile_args.extend(cov)
+    extra_link_args.extend(cov)
 if DEBUG:
     extra_compile_args.extend(["-O0", "-g", "-UNDEBUG"])
 
@@ -51,71 +61,17 @@ if sys.platform == "win32":
 ext_modules = [
     Extension(
         "msgspec._core",
-        [os.path.join("msgspec", "_core.c")],
+        [os.path.join("src", "msgspec", "_core.c")],
         extra_compile_args=extra_compile_args,
         extra_link_args=extra_link_args,
     )
 ]
 
-yaml_deps = ["pyyaml"]
-toml_deps = ['tomli ; python_version < "3.11"', "tomli_w"]
-doc_deps = ["sphinx", "furo", "sphinx-copybutton", "sphinx-design", "ipython"]
-test_deps = [
-    "pytest",
-    "msgpack",
-    "attrs",
-    'eval-type-backport ; python_version < "3.10"',
-    *yaml_deps,
-    *toml_deps,
-]
-dev_deps = ["pre-commit", "coverage", "mypy", "pyright", *doc_deps, *test_deps]
-
-extras_require = {
-    "yaml": yaml_deps,
-    "toml": toml_deps,
-    "doc": doc_deps,
-    "test": test_deps,
-    "dev": dev_deps,
-}
-
 setup(
     name="msgspec",
     version=versioneer.get_version(),
     cmdclass=versioneer.get_cmdclass(),
-    maintainer="Jim Crist-Harif",
-    maintainer_email="jcristharif@gmail.com",
-    url="https://jcristharif.com/msgspec/",
-    project_urls={
-        "Documentation": "https://jcristharif.com/msgspec/",
-        "Source": "https://github.com/jcrist/msgspec/",
-        "Issue Tracker": "https://github.com/jcrist/msgspec/issues",
-    },
-    description=(
-        "A fast serialization and validation library, with builtin support for "
-        "JSON, MessagePack, YAML, and TOML."
-    ),
-    keywords="JSON msgpack MessagePack TOML YAML serialization validation schema",
-    classifiers=[
-        "License :: OSI Approved :: BSD License",
-        "Development Status :: 4 - Beta",
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.10",
-        "Programming Language :: Python :: 3.11",
-        "Programming Language :: Python :: 3.12",
-        "Programming Language :: Python :: 3.13",
-        "Programming Language :: Python :: 3.14",
-    ],
-    extras_require=extras_require,
-    license="BSD",
     packages=["msgspec"],
-    package_data={"msgspec": ["py.typed", "*.pyi"]},
+    package_dir={"": "src"},
     ext_modules=ext_modules,
-    long_description=(
-        open("README.md", encoding="utf-8").read()
-        if os.path.exists("README.md")
-        else ""
-    ),
-    long_description_content_type="text/markdown",
-    python_requires=">=3.9",
-    zip_safe=False,
 )
