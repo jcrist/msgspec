e2d8692e030488569a14daca9b5e17e4c4f7a818
Ofek Lev <ofekmeister@gmail.com>
2025-10-28 18:04:06 -0400

    Statically define project metadata

--- setup.py
+++ setup.py
@@ -55,63 +55,10 @@ ext_modules = [
     )
 ]
 
-yaml_deps = ["pyyaml"]
-toml_deps = ['tomli ; python_version < "3.11"', "tomli_w"]
-doc_deps = ["sphinx", "furo", "sphinx-copybutton", "sphinx-design", "ipython"]
-test_deps = [
-    "pytest",
-    "msgpack",
-    "attrs",
-    'eval-type-backport ; python_version < "3.10"',
-    *yaml_deps,
-    *toml_deps,
-]
-dev_deps = ["pre-commit", "coverage", "mypy", "pyright", *doc_deps, *test_deps]
-
-extras_require = {
-    "yaml": yaml_deps,
-    "toml": toml_deps,
-    "doc": doc_deps,
-    "test": test_deps,
-    "dev": dev_deps,
-}
-
 setup(
     name="msgspec",
-    maintainer="Jim Crist-Harif",
-    maintainer_email="jcristharif@gmail.com",
-    url="https://jcristharif.com/msgspec/",
-    project_urls={
-        "Documentation": "https://jcristharif.com/msgspec/",
-        "Source": "https://github.com/jcrist/msgspec/",
-        "Issue Tracker": "https://github.com/jcrist/msgspec/issues",
-    },
-    description=(
-        "A fast serialization and validation library, with builtin support for "
-        "JSON, MessagePack, YAML, and TOML."
-    ),
-    keywords="JSON msgpack MessagePack TOML YAML serialization validation schema",
-    classifiers=[
-        "License :: OSI Approved :: BSD License",
-        "Development Status :: 4 - Beta",
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.10",
-        "Programming Language :: Python :: 3.11",
-        "Programming Language :: Python :: 3.12",
-        "Programming Language :: Python :: 3.13",
-        "Programming Language :: Python :: 3.14",
-    ],
-    extras_require=extras_require,
     license="BSD",
     packages=["msgspec"],
     package_data={"msgspec": ["py.typed", "*.pyi"]},
     ext_modules=ext_modules,
-    long_description=(
-        open("README.md", encoding="utf-8").read()
-        if os.path.exists("README.md")
-        else ""
-    ),
-    long_description_content_type="text/markdown",
-    python_requires=">=3.9",
-    zip_safe=False,
 )
