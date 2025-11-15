b23fa716597bb30a0970ba34b97b177db5f6d2bc
Ofek Lev <ofekmeister@gmail.com>
2025-11-15 09:52:18 -0500

    Update test workflow (#932)

--- setup.py
+++ setup.py
@@ -36,6 +36,8 @@ if COVERAGE:
     extra_link_args.append("-lgcov")
 if DEBUG:
     extra_compile_args.extend(["-O0", "-g", "-UNDEBUG"])
+elif sys.platform != "win32":
+    extra_compile_args.extend(["-g0"])
 
 # from https://py-free-threading.github.io/faq/#im-trying-to-build-a-library-on-windows-but-msvc-says-c-atomic-support-is-not-enabled
 if sys.platform == "win32":
